92ee5ce3596ce4a6ed4db9910e16227996b6e549
Jack <jack@easyapple.net>
2019-12-20 11:50:31 +0800

    Update lc0062.cpp

--- MySrc/lc0062.cpp
+++ MySrc/lc0062.cpp
@@ -1,5 +1,9 @@
 // https://leetcode-cn.com/problems/unique-paths/
 
+// 方法一：排列组合，左上到右下共需向右m-1步，向下n-1步，计算C((m+2-2),(m-1))即可
+// C(n,m) = C(n,(n-m)) = P(n,m)/m! = n!/((n-m)!*m!)
+// P(n,m) = n!/(n-m)!
+// 注意：阶乘需要考虑结果较大的情况，可以使用double类型
 class Solution {
 private:
     int combination(int n, int m) {
