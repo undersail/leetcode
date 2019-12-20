06ad67f826c736f1619e9467cc29ebde0aa75f23
Jack <jack@easyapple.net>
2019-12-20 15:41:46 +0800

    Update lc0134.cpp

--- MySrc/lc0134.cpp
+++ MySrc/lc0134.cpp
@@ -1,49 +1,42 @@
 // https://leetcode-cn.com/problems/gas-station/
 
+// 暴力解法，注意暴力解法用时较多，预先计算gas.size()可节省一半时间
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int gasSize = gas.size();
         int gasSum = 0;
         int costSum = 0;
         int startNum = 0;
-        int num = gas.size();
-        int curIdx = 0;
-        int i = 0;
 
-        /*for (; i < num; i++){
-            gasSum += gas[i];
-            costSum += cost[i];
-        }
-        if(gasSum < costSum) 
-            return -1;*/
-        
-        while(startNum < num){            
-            if(gas[startNum] < cost[startNum]){
+        while(startNum < gasSize) {
+            // 找起点
+            if(gas[startNum] < cost[startNum]) {
                 startNum++;
-            }
-            else{
+            } else {
                 gasSum = 0;
                 costSum = 0;
-                for(i = 0; i < num; i++){
-                    curIdx = (startNum + i) % num;
-                    gasSum += gas[curIdx];
-                    costSum += cost[curIdx];
-                
+                // 尝试走完一圈
+                int index = 0;
+                for (index = 0; index < gasSize; index++) {
+                    // 当前加油站的序号
+                    int curNum = (startNum + index) % gasSize;
+                    gasSum += gas[curNum];
+                    costSum += cost[curNum];
                     if(gasSum < costSum){
                         startNum++;
                         break;
                     }
-                } 
-                if(i < num) 
-                    continue;
-                else
+                }
+                // 注意把此处判断放进循环中会导致超时
+                if (index == gasSize) {
                     break;
+                }
             }
         }
-        
-        if(startNum == num)
-            return -1;
-        else
+        if (startNum < gasSize) {
             return startNum;
+        }
+        return -1;  
     }
 };
