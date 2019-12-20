e0a5f09ec3509ddbab898f96473f49995e866eb7
Jack <jack@easyapple.net>
2019-12-20 14:54:07 +0800

    Update lc0102.cpp

--- MySrc/lc0102.cpp
+++ MySrc/lc0102.cpp
@@ -9,7 +9,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 二叉树，层序遍历
 class Solution {
+
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> result;
+        int index = 0;
+        // 递归，index表示层序号
+        traverse(result, root, index);
+        return result;
+    }
+
 private:
     void traverse(vector<vector<int>>& result, TreeNode* node, int& index)
     {
@@ -23,16 +35,10 @@ private:
         } else {
             result[index].push_back(node->val);
         }
+        // 关键点：子节点层序号+1
         int index1 = index + 1;
         int index2 = index + 1;
         traverse(result, node->left, index1);
         traverse(result, node->right, index2);
-    }
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> result;
-        int index = 0;
-        traverse(result, root, index);
-        return result;
     }
 };
