8851f28a60c1b89f5e29dc657b9b9085fc9e24de
Jack <jack@easyapple.net>
2019-12-20 13:44:53 +0800

    Update lc0064.cpp

--- MySrc/lc0064.cpp
+++ MySrc/lc0064.cpp
@@ -1,5 +1,6 @@
 // https://leetcode-cn.com/problems/minimum-path-sum/
 
+// 动态规划：dp[i][j]记录从(i,j)到右下角的路径最小和
 class Solution {
 
 public:
@@ -11,16 +12,19 @@ public:
         int n = grid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
         dp[m - 1][n - 1] = grid[m - 1][n - 1];
-        for (int x = m - 2; x >= 0; x--) {
-           dp[x][n - 1] = grid[x][n - 1] + dp[x + 1][n - 1]; 
+        // 最后一列，只能向下走
+        for (int i = m - 2; i >= 0; i--) {
+           dp[i][n - 1] = grid[i][n - 1] + dp[i + 1][n - 1]; 
         }
-        for (int y = n - 2; y >= 0; y--) {
-           dp[m - 1][y] = grid[m - 1][y] + dp[m - 1][y + 1]; 
+        // 最后一行，只能向右走
+        for (int j = n - 2; j >= 0; j--) {
+           dp[m - 1][j] = grid[m - 1][j] + dp[m - 1][j + 1]; 
         }
-        for (int x = m - 2; x >= 0; x--) {
-            for (int y = n - 2; y >= 0; y--) {
-                if (dp[x][y] < 0) {
-                    dp[x][y] = grid[x][y] + min(dp[x + 1][y], dp[x][y + 1]);
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                if (dp[i][j] < 0) {
+                    // 任意一点只能向右或向下走
+                    dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1]);
                 }
             }
         }
