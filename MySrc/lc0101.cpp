28cba54d50a13a49b3e527598790a0edec3e5c1e
Jack <jack@easyapple.net>
2019-12-20 14:40:33 +0800

    Update lc0101.cpp

--- MySrc/lc0101.cpp
+++ MySrc/lc0101.cpp
@@ -9,15 +9,19 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// 二叉树，递归
 class Solution {
 private:
     bool IsMirror(TreeNode* node1, TreeNode* node2)
     {
+        // 处理特殊情况
         if (node1 == NULL && node2 == NULL) {
             return true;
         } if ((node1 == NULL && node2 != NULL) || (node1 != NULL && node2 == NULL)) {
             return false;
         } else {
+            // 关键点：val相等且左-左与右-右对称、左-右与右-左对称
             return (IsMirror(node1->left, node2->right) && IsMirror(node1->right, node2->left) && node1->val == node2->val);
         }
     }
